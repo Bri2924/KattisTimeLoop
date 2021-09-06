da46e4b8d21b0787220f0ea03085e75b3fecced7
bpena <bpena@ONMED-IT>
2021-09-05 22:03:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class timeloop
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+        for (int x = 1; x <= num; x++)
+            Console.WriteLine(x + " Abracadabra");
+    }
+}
